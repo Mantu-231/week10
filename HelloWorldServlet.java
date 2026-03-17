e4f153454a9bd313a518496040c63289a4aef50e
Mantu-231 <mkumar12@gitam.in>
2026-03-17 23:16:58 +0530

    Update HelloWorldServlet.java

--- HelloWorldServlet.java
+++ HelloWorldServlet.java
@@ -2,20 +2,14 @@ import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 
-public class HelloWorldServlet extends HttpServlet
-{
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response)
-                      throws ServletException, IOException
-    {
+public class HelloWorldServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
         response.setContentType("text/html");
-
         PrintWriter out = response.getWriter();
-
-        out.println("<html>");
-        out.println("<body>");
-        out.println("<h1>Hello World</h1>");
-        out.println("</body>");
-        out.println("</html>");
+        out.println("<html><body>");
+        out.println("<h2>Hello World from Servlet!</h2>");
+        out.println("</body></html>");
     }
 }
